d6b48e8375e4ebdbeeab9bcbe6bf04fea495d7bf
log-game <ermolaevaalina97@gmail.com>
2026-02-18 23:10:29 +0300

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -1,6 +1,16 @@
-let messages = global.messages || [];
-global.messages = messages;
+import { createClient } from "@supabase/supabase-js";
 
-export default function handler(req, res) {
-  res.status(200).json(messages.slice(-100));
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+  const { data } = await supabase
+    .from("messages")
+    .select("*")
+    .order("id", { ascending: true })
+    .limit(100);
+
+  res.status(200).json(data);
 }
